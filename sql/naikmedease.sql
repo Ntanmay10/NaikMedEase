9e16b443002c8fae9b23b5df537c454a9e462ecb
Ntanmay10 <findtanmay10@gmail.com>
2024-03-22 22:54:12 +0530

    22-03-2024

--- sql/naikmedease.sql
+++ sql/naikmedease.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 21, 2024 at 05:56 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 22, 2024 at 05:23 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,12 +27,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `cart`
 --
 
-CREATE TABLE `cart` (
-  `cartid` int(5) NOT NULL,
-  `prdid` int(5) NOT NULL,
-  `regid` int(5) NOT NULL,
-  `quantity` int(5) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE IF NOT EXISTS `cart` (
+  `cartid` int NOT NULL AUTO_INCREMENT,
+  `prdid` int NOT NULL,
+  `regid` int NOT NULL,
+  `quantity` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`cartid`)
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -40,40 +42,24 @@ CREATE TABLE `cart` (
 -- Table structure for table `company`
 --
 
-CREATE TABLE `company` (
-  `compid` int(5) NOT NULL,
-  `compname` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `company`;
+CREATE TABLE IF NOT EXISTS `company` (
+  `compid` int NOT NULL AUTO_INCREMENT,
+  `compname` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`compid`),
+  UNIQUE KEY `compname` (`compname`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `company`
 --
 
 INSERT INTO `company` (`compid`, `compname`) VALUES
-(13, 'Abbota'),
-(26, 'Albatros'),
-(14, 'apollopharma'),
-(21, 'beardo'),
-(23, 'Chiragpharma&co'),
-(12, 'Cipla ltd.'),
-(4, 'dabur'),
-(25, 'Delta pharma '),
-(16, 'Dove'),
-(22, 'Dove india'),
-(17, 'Ghar soap'),
-(11, 'indianpharma'),
-(3, 'intas'),
-(20, 'leeford'),
-(24, 'Malipharma&co'),
-(8, 'mankind'),
-(2, 'naikdrugs'),
-(9, 'nationalmeds'),
-(7, 'patanjali'),
-(1, 'patelpharma'),
-(15, 'perfora'),
-(10, 'sunpharma'),
-(18, 'Torrent pharma'),
-(19, 'wildstone');
+(4, 'Abotta'),
+(2, 'Beardo'),
+(3, 'Cipla'),
+(1, 'Dove'),
+(5, 'Sunpharma');
 
 -- --------------------------------------------------------
 
@@ -81,14 +67,26 @@ INSERT INTO `company` (`compid`, `compname`) VALUES
 -- Table structure for table `contact`
 --
 
-CREATE TABLE `contact` (
-  `cntid` int(5) NOT NULL,
-  `cntname` varchar(25) NOT NULL,
-  `cntemail` varchar(50) NOT NULL,
-  `cntdesc` varchar(250) NOT NULL,
-  `cntcode` int(7) NOT NULL,
-  `cntstatus` varchar(200) NOT NULL DEFAULT 'pending'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `cntid` int NOT NULL AUTO_INCREMENT,
+  `cntname` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
+  `cntemail` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `cntdesc` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
+  `cntcode` int NOT NULL,
+  `cntstatus` varchar(200) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'pending',
+  PRIMARY KEY (`cntid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`cntid`, `cntname`, `cntemail`, `cntdesc`, `cntcode`, `cntstatus`) VALUES
+(1, 'Yash Mehta', 'yash@gmail.com', 'I want diprolite', 8539, 'pending'),
+(2, 'meet patel', 'meet@gmail.com', 'I want azitromycyne', 2435, 'pending'),
+(3, 'Dhruv patel', 'dhruv@gmail.com', 'I want soframycine', 8132, 'pending'),
+(4, 'Poojan Naik', 'Poojan@gmail.com', 'I want salisia KT', 7055, 'pending');
 
 -- --------------------------------------------------------
 
@@ -96,12 +94,23 @@ CREATE TABLE `contact` (
 -- Table structure for table `feedback`
 --
 
-CREATE TABLE `feedback` (
-  `feedid` int(5) NOT NULL,
-  `feedname` varchar(20) NOT NULL,
-  `feedemail` varchar(50) NOT NULL,
-  `feeddesc` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `feedback`;
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `feedid` int NOT NULL AUTO_INCREMENT,
+  `feedname` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `feedemail` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `feeddesc` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`feedid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`feedid`, `feedname`, `feedemail`, `feeddesc`) VALUES
+(1, 'Hardik Desai', 'hardik@gmail.com', 'Well Done'),
+(2, 'Ashish Patel', 'ashish@gmail.com', 'Keep it Up'),
+(3, 'Sunil Naik', 'Sunil@gmail.com', 'Nicee');
 
 -- --------------------------------------------------------
 
@@ -109,13 +118,30 @@ CREATE TABLE `feedback` (
 -- Table structure for table `ordertab`
 --
 
-CREATE TABLE `ordertab` (
-  `orderid` int(5) NOT NULL,
-  `regid` int(5) NOT NULL,
-  `orddate` varchar(12) NOT NULL,
-  `prdid` int(5) NOT NULL,
-  `qty` int(5) NOT NULL
+DROP TABLE IF EXISTS `ordertab`;
+CREATE TABLE IF NOT EXISTS `ordertab` (
+  `orderid` int NOT NULL AUTO_INCREMENT,
+  `regid` int NOT NULL,
+  `orddate` varchar(12) COLLATE utf8mb4_general_ci NOT NULL,
+  `prdid` int NOT NULL,
+  `qty` int NOT NULL,
+  PRIMARY KEY (`orderid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE IF NOT EXISTS `payment` (
+  `payid` int NOT NULL AUTO_INCREMENT,
+  `transcode` int NOT NULL,
+  `amount` varchar(5) NOT NULL,
+  `regid` int NOT NULL,
+  PRIMARY KEY (`payid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -123,14 +149,27 @@ CREATE TABLE `ordertab` (
 -- Table structure for table `product`
 --
 
-CREATE TABLE `product` (
-  `prdid` int(5) NOT NULL,
-  `prdnm` varchar(50) NOT NULL,
-  `prdpri` varchar(10) NOT NULL,
-  `compid` int(5) NOT NULL,
-  `prdimg` varchar(100) NOT NULL,
-  `stock` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `prdid` int NOT NULL AUTO_INCREMENT,
+  `prdnm` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `prdpri` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `compid` int NOT NULL,
+  `prdimg` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `stock` int NOT NULL,
+  PRIMARY KEY (`prdid`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`prdid`, `prdnm`, `prdpri`, `compid`, `prdimg`, `stock`) VALUES
+(1, 'Anti Dandruff shampoo', '295', 2, 'antidandruf.jpg', 10),
+(2, 'Nicotex', '150', 3, 'nicotex.jpeg', 15),
+(3, 'Diprolite', '100', 4, 'diprolite.jpeg', 20),
+(4, 'Powder', '80', 4, 'powder.png', 10),
+(5, 'Cetaphil', '480', 4, 'cetaphil.jpg', 30);
 
 -- --------------------------------------------------------
 
@@ -138,15 +177,19 @@ CREATE TABLE `product` (
 -- Table structure for table `registration`
 --
 
-CREATE TABLE `registration` (
-  `regid` int(5) NOT NULL,
-  `fullnm` varchar(50) NOT NULL,
-  `usernm` varchar(15) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `passwd` varchar(15) NOT NULL,
-  `usertyp` varchar(5) DEFAULT 'User',
-  `secucode` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `registration`;
+CREATE TABLE IF NOT EXISTS `registration` (
+  `regid` int NOT NULL AUTO_INCREMENT,
+  `fullnm` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `usernm` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `passwd` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `usertyp` varchar(5) COLLATE utf8mb4_general_ci DEFAULT 'User',
+  `secucode` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`regid`),
+  UNIQUE KEY `usernm` (`usernm`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `registration`
@@ -154,102 +197,7 @@ CREATE TABLE `registration` (
 
 INSERT INTO `registration` (`regid`, `fullnm`, `usernm`, `email`, `passwd`, `usertyp`, `secucode`) VALUES
 (1, 'Tanmay Naik', 'Admin', 'findtanmay10@gmail.com', 'Admin@2510', 'Admin', '2510'),
-(2, 'Mahek Naik', 'Mahek21', 'Maheknaik021@gmail.com', 'Mahek@2110', 'User', '2110');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `cart`
---
-ALTER TABLE `cart`
-  ADD PRIMARY KEY (`cartid`);
-
---
--- Indexes for table `company`
---
-ALTER TABLE `company`
-  ADD PRIMARY KEY (`compid`),
-  ADD UNIQUE KEY `compname` (`compname`);
-
---
--- Indexes for table `contact`
---
-ALTER TABLE `contact`
-  ADD PRIMARY KEY (`cntid`);
-
---
--- Indexes for table `feedback`
---
-ALTER TABLE `feedback`
-  ADD PRIMARY KEY (`feedid`);
-
---
--- Indexes for table `ordertab`
---
-ALTER TABLE `ordertab`
-  ADD PRIMARY KEY (`orderid`);
-
---
--- Indexes for table `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`prdid`);
-
---
--- Indexes for table `registration`
---
-ALTER TABLE `registration`
-  ADD PRIMARY KEY (`regid`),
-  ADD UNIQUE KEY `usernm` (`usernm`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `cart`
---
-ALTER TABLE `cart`
-  MODIFY `cartid` int(5) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `company`
---
-ALTER TABLE `company`
-  MODIFY `compid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
-
---
--- AUTO_INCREMENT for table `contact`
---
-ALTER TABLE `contact`
-  MODIFY `cntid` int(5) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `feedback`
---
-ALTER TABLE `feedback`
-  MODIFY `feedid` int(5) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `ordertab`
---
-ALTER TABLE `ordertab`
-  MODIFY `orderid` int(5) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-  MODIFY `prdid` int(5) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `registration`
---
-ALTER TABLE `registration`
-  MODIFY `regid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+(2, 'Mahek Naik', 'Mahek21', 'maheknaik021@gmail.com', 'Mahek@2110', 'User', '2110');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
