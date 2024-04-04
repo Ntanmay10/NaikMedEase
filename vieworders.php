ee3b887f3c91b2e437bcba6b4b1f4de6fb6ff8d4
Ntanmay10 <findtanmay10@gmail.com>
2024-04-04 19:51:56 +0530

    04-04-2024

--- vieworders.php
+++ vieworders.php
@@ -15,6 +15,11 @@
         header('location:checkpres.php');
     }
 
+    if (isset($_POST['btnprod'])) {
+        $_SESSION['order_id']=$_REQUEST['btnprod'];
+        header('location:checkprod.php');
+    }
+
     if (isset($_POST['btnconfirm'])) {
         $order_id=$_REQUEST['btnconfirm'];
         $updstatus=mysqli_query($con,"UPDATE orders set ord_status='done' where order_id='$order_id'");
@@ -22,14 +27,11 @@
     }
     ?>
     <link rel="stylesheet" href="./css/vieworder.css">
-    <link rel="stylesheet" href="./css/form.css">
-
 </head>
 
 <body>
-    <form action="" method="post">
-
-        <div class="container">
+    <form method="post">
+        <div  class="container">
             <h2>Pending Orders</h2>
             <table class="table table-hover">
                 <thead class="table-dark">
@@ -39,38 +41,44 @@
                         <th>Name</th>
                         <th>Email</th>
                         <th>Amount</th>
+                        <th>Address</th>
                         <th>Ref ID</th>
-                        <th>Prescription</th>
                         <th>Status</th>
+                        <th>Products</th>
+                        <th>Prescription</th>
                         <th>Confirm Order</th>
                     </tr>
                 </thead>
                 <tbody id="myTable">
                     <?php
-                    $tot = 0;
+                    $n=1;
                     while ($rw = mysqli_fetch_array($query)) {
                         $getuser = mysqli_query($con, "SELECT * FROM registration  WHERE regid='$rw[customer_id]'");
                         while ($row = mysqli_fetch_array($getuser)) {
                             $pay=mysqli_query($con,"SELECT * FROM payment WHERE order_id='$rw[order_id]'");
                             while ($payrow=mysqli_fetch_array($pay)) {
                                 echo "<tr>
-                                <td> ". $rw['order_id'] ." </td>
+                                <td> $n </td>
                             <td> ". $rw['order_date'] ." </td>
                             <td> ". $row['fullnm'] ." </td>
                             <td> ". $row['email'] ." </td>
                             <td> ". $rw['total_amount']  ." </td>
+                            <td> ". $rw['addrs']  ." </td>
                             <td> ". $payrow['transcode']  ." </td>
-                            <td><button type='submit' name='btnpres' value=".$rw['order_id']." class='btn btn-primary'>Check Prescription</button></td>
                             <td> ". $rw['ord_status']  ." </td>
-                            <td><button type='submit' name='btnconfirm' value=".$rw['order_id']." class='btn btn-success'>Dispatch order</button></td>
+                            <td><button type='submit' name='btnprod' value=".$rw['order_id']." class='btn btn-primary'>View</button></td>
+                            <td><button type='submit' name='btnpres' value=".$rw['order_id']." class='btn btn-primary'>Check</button></td>
+                            <td><button type='submit' name='btnconfirm' value=".$rw['order_id']." class='btn btn-success'>Dispatch</button></td>
                             </tr>";
                         }
                         }
+                        $n++;
                     }
                     ?>
                 </tbody>
             </table>
         </div>
+    </form>
         <script src="./js/nav.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
@@ -85,7 +93,6 @@
                 });
             });
         </script>
-    </form>
 
 </body>
 
